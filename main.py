b2ada1af19d4c76b2b280631d29932fc25f94f04
Arvid Reinert <arvid.reinert@gmail.com>
2024-07-18 20:13:00 +0200

    WIP

--- main.py
+++ main.py
@@ -31,10 +31,10 @@ class game():
             self.boat.set_rotation(self.boat_speed[1])
     
             screen.fill((0,0,0))
-            self.background.update()
-            self.tutorial_view.update()
-            self.boat.update()
-            self.water_shower.update()
+            self.background.update(screen)
+            self.tutorial_view.update(screen)
+            self.boat.update(screen)
+            self.water_shower.update(screen)
             pygame.display.update()
 
 #main menu:
@@ -45,7 +45,7 @@ running = True
 while running == True:
     clock.tick(30)
     mous_pos = pygame.mouse.get_pos()
-    if play_button.get_point_colide(mous_pos):
+    if play_button.get_point_collide(mous_pos):
         play_button.set_transparency(100)
     else:
         play_button.set_transparency(255)
@@ -55,14 +55,14 @@ while running == True:
             sys.exit()
         #here is the button
         if event.type == pygame.MOUSEBUTTONDOWN:
-            if play_button.get_point_colide(mous_pos):
+            if play_button.get_point_collide(mous_pos):
                 print("click")
                 screen.fill((0,0,0))
                 running = "play"
 
     if running == True:
-        background.update()
-        play_button.update()
+        background.update(screen)
+        play_button.update(screen)
         pygame.display.update()
 
 print(running)
--- rectangle.py
+++ rectangle.py
@@ -1,82 +1,71 @@
 from setup import *
+import pygame
+
 class Rectangle():
-    def __init__(self, size, position, color, image):
-        self.image = image
+    def __init__(self, size, position, color, image=None):
+        self.image_path = image
         self.rotation = 0
         self.is_updating = True
         self.size = size
         self.position = position
         self.transparency = 255
         self.color = color
+        
         if not image:
             self.rect = pygame.Surface(size)
             self.rect.fill(self.color)
-            self.position = position
-            self.rect_rect = self.rect.get_rect()
-            #self.rect_rect.center = self.position[0] - self.size[0] / 2, self.position[1] - self.size[1] / 2
-            self.position = self.rect_rect.center
+            self.rect.set_alpha(self.transparency)
+            self.rect_rect = self.rect.get_rect(center=position)
         else:
-            self.position = position
             self.image = pygame.image.load(image).convert_alpha()
             self.image = pygame.transform.scale(self.image, size)
-            self.image_rect = self.image.get_rect()
-            self.image_rect.center = self.position[0]-self.size[0],self.position[1]-self.size[1]
-            self.position = self.image_rect.center
+            self.image.set_alpha(self.transparency)
+            self.image_rect = self.image.get_rect(center=position)
         
-
-    def update(self):
+    def update(self, surface):
         if self.is_updating:
-            surface.fill((0, 0, 0, 0))  # Clear the surface
-            #some transforming:
-            col = list(self.color)
-            col.append(self.transparency)
-            col = tuple(col)
-            surface.set_alpha(self.transparency)
-            if self.image != False:
+            if self.image_path is not None:
                 self.image_rect.center = self.position
-                surface.blit(self.image, self.image_rect.center)
+                surface.blit(self.image, self.image_rect)
             else:
                 self.rect_rect.center = self.position
-                #self.position[0]-self.size[0], self.position[1]-self.size[1]
-                surface.blit(self.rect, self.image_rect.center)
-            screen.blit(surface, (0, 0))
-
+                surface.blit(self.rect, self.rect_rect)
+        
     def set_transparency(self, transparency):
         self.transparency = transparency
+        if self.image_path is not None:
+            self.image.set_alpha(transparency)
+        else:
+            self.rect.set_alpha(transparency)
 
-    def set_position(self,xc,yc):
-        self.position = xc, yc
-        self.update()
+    def set_position(self, xc, yc):
+        self.position = (xc, yc)
 
     def change_position(self, xc, yc):
-        self.position = self.position[0]+xc,self.position[1]+yc
-        self.update()
+        self.position = (self.position[0] + xc, self.position[1] + yc)
 
     def kill(self):
         self.is_updating = False
 
     def set_rotation(self, rot):
-        if self.image == False:
-            self.rect = pygame.transform.rotate(self.rect, rot)
-            self.rect_rect = self.rect.get_rect(center = self.rect_rect.center)
+        if self.image_path is not None:
+            self.image = pygame.transform.rotate(self.image, rot)
+            self.image_rect = self.image.get_rect(center=self.position)
         else:
-           self.image = pygame.transform.rotate(self.image, rot)
-           self.image_rect = self.image.get_rect(center = self.image_rect.center)
+            self.rect = pygame.transform.rotate(self.rect, rot)
+            self.rect_rect = self.rect.get_rect(center=self.position)
 
     def get_pos(self):
-        if self.image == False:
-            return self.position
-        else:
-            return self.position
+        return self.position
 
-    def get_point_colide(self,point):
-        if self.image != False:
+    def get_point_collide(self, point):
+        if self.image_path is not None:
             return self.image_rect.collidepoint(point)
         else:
             return self.rect_rect.collidepoint(point)
         
-    def get_colliding_with(self,colrect):
-        if self.image == False:
-            return self.rect_rect.colliderect(colrect.rect_rect if not colrect.image else colrect.image_rect)
+    def get_colliding_with(self, colrect):
+        if self.image_path is not None:
+            return self.image_rect.colliderect(colrect.image_rect if colrect.image_path is not None else colrect.rect_rect)
         else:
-            return self.image_rect.colliderect(colrect.image_rect if not colrect.image else colrect.image_rect)
+            return self.rect_rect.colliderect(colrect.rect_rect if colrect.image_path is None else colrect.image_rect)
